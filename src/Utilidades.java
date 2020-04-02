e17511528fd1021f963c007c10a24448ccbcf930
Montse <montsemsanz@gmail.com>
2020-04-02 16:54:23 +0200

    Añadido Proyecto de partida para Entrega 1 UT7 Empresa alquiler de vehículos Fase1

---
+++ src/Utilidades.java
@@ -0,0 +1,30 @@
+
+
+public class Utilidades {
+
+	public static String[] obtenerLineasDatos() {
+		return new String[] { "C, 8792 ANV,  Fiat ,  500 , 20, 4",
+		        "F, 1267 BBC ,  Renault ,  Trafic , 36, 4.2",
+		        "F, 3456 aaa ,  mercedes ,  sprinter 3 , 39, 11.6",
+		        "C, 2367 CXB,  Volkswagen ,  Polo , 26, 5",
+		        "F, 7577 aam,citroen,berlingo, 35, 4.3",
+		        "F, 4355 jka,citroen,jumpy , 31, 6.6",
+		        "C, 4222 bxs,  seat ,  ibiza , 25, 5",
+		        "C, 6663 vdg,  Audi ,  a3 , 36, 5",
+		        "F, 7577 aam,citroen,berlingo, 35, 4.3",
+		        "C, 2216 MCD,  Volkswagen ,  turan , 32, 5",
+		        "C, 3214 AAA,  Volkswagen ,  beetle , 25, 4",
+		        "C, 6663 vdg,  Audi ,  a3 , 36, 5",
+		        "F, 1267 BBC ,  Renault ,  Trafic , 36, 4.2",
+		        "F, 3456 aaa ,  mercedes ,  sprinter 3 , 39, 11.6",
+		        "C, 6663 vdg,  Audi ,  a3 , 36, 5",
+		        "F, 3456 aaa ,  mercedes ,  sprinter 3 , 39, 11.6",
+		        "F, 1933 ccd,Mercedes,Citan 109, 35, 3.2",
+		        "C, 8792 ANV,  Fiat ,  500 , 20, 4",
+		        "F, 7577 aam,citroen,berlingo, 35, 4.3",
+		        "C, 8891 CXU,  Audi ,  cabrio , 33, 4",
+		        "F, 4355 jka,citroen,jumpy , 31, 6.6" };
+
+	}
+
+}
